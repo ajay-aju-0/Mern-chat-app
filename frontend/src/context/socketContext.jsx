6dddd097458b1d13827a555c0d254b2a3a5d439a
ajayaju <ajaypadmanabhan01@gmail.com>
2024-07-04 12:01:50 +0530

    Final deployment

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8080", {
+			const socket = io("https://mern-chat-app-pug4.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
